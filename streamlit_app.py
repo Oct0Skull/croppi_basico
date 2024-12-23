2f0ffa179781e228370511b697dc3b86b628d243
Octo_Skull <17036615+Oct0Skull@users.noreply.github.com>
2024-12-23 06:35:04 +0000

    add buttoms wuth FOR cicle

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,16 @@
 import streamlit as st
 
-st.title("🎈 My new app")
+st.title("🏃‍♂️💨 Croppi app")
+
+# mensaje de bienvenida por default
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
+# ejemplo tomado de Youtube
+cantidad = st.slider('Seleccionar la cantidad:')
+
+st.write('La cantidad seleccionada es: {cantidad}')
+
+# se eneran una cantidad de botones de acuerdo al numero colocado
+for i in range(cantidad):
+    st.button(f'boton {i+1}')
